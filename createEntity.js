c51152494ae730d2a8c509127d5b9287bc99164d
Matías Fuentes <matifuentes2@gmail.com>
2025-02-13 14:44:28 -0300

    test create entity

--- createEntity.js
+++ createEntity.js
@@ -1,11 +1,25 @@
 const Fibery = require('fibery-unofficial');
-const fibery = new Fibery({host: "blueprint.fibery.io", token: process.env.FIBERY_TOKEN});
 
-await fibery.entity.createBatch([
-  {
-    'type': 'Test Space/Test DB',
-    'entity': {
-      'Test Space/Name': 'hola'
-    }
+const fibery = new Fibery({
+  host: "blueprint.fibery.io",
+  token: process.env.FIBERY_TOKEN
+});
+
+async function createPlayer() {
+  try {
+    await fibery.entity.createBatch([
+      {
+        'type': 'Test Space/Person',
+        'entity': {
+          'Test Space/Name': 'Curtly Ambrose',
+        }
+      }
+    ]);
+
+    console.log("Player created successfully!");
+  } catch (error) {
+    console.error("Error creating player:", error);
   }
-]);
+}
+
+createPlayer();
